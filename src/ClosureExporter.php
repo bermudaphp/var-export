93db956f1f02715c67b048f9c76900d9454eeb87
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-07 20:40:15 +0200

    Update ClosureExporter.php

--- src/ClosureExporter.php
+++ src/ClosureExporter.php
@@ -11,18 +11,18 @@ use PhpParser\ParserFactory;
 use PhpParser\PrettyPrinter\Standard;
 use PhpParser\PrettyPrinterAbstract;
 
-class ClosureExporter implements ClosureExporterInterface {
+class ClosureExporter {
     public function exportClosure(\Closure $closure): string
     {
         $reflector = $this->getReflector($closure);
         $parser = $this->createParser();
-        
+
         $code = $this->readFile($reflector->getFileName());
-        
+
         $ast = $parser->parse($code);
 
         $nodeFinder = new NodeFinder;
-        
+
         $node = $nodeFinder->findFirst($ast, $this->createFindCallback($reflector));
 
         if ($reflector->getReturnType()) {
@@ -39,34 +39,33 @@ class ClosureExporter implements ClosureExporterInterface {
 
         return $this->getPrinter()->prettyPrintExpr($node);
     }
-    
+
     protected function readFile(string $filename): string {
         $fh = fopen($filename, 'r');
         $content = '';
-        
+
         while (!feof($fh)) {
             $content .= fgets($fh);
         }
-        
+
         fclose($fh);
         return $content;
     }
-    
     protected function createParser(): Parser
     {
         return (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
     }
-    
+
     protected function getReflector(\Closure $closure): \ReflectionFunction
     {
         return new \ReflectionFunction($closure);
     }
-    
+
     protected function getPrinter(): PrettyPrinterAbstract
     {
         return (new Standard);
     }
-    
+
     protected function createFindCallback(\ReflectionFunction $reflector): callable
     {
         return static function(Node $node) use ($reflector): bool {
