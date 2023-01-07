9cd2fdb53b4cbfdfc086fed830f464eab4e15d32
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-07 21:23:26 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,69 +1,22 @@
 <?php
 
-namespace Bermuda\Utils;
+namespace Bermuda\VarExporter;
 
-use PhpParser\Node;
-use PhpParser\Node\Expr\ArrowFunction as A;
-use PhpParser\Node\Expr\Closure as C;
-use PhpParser\NodeFinder;
-use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitorAbstract;
-use PhpParser\ParserFactory;
-use PhpParser\PrettyPrinter\Standard;
-
-function closureToString(\Closure $closure): string
+/**
+ * @param \Closure $closure
+ * @return string
+ */
+function export_closure(\Closure $closure): string
 {
-    $reflector = new \ReflectionFunction($closure);
+    return (new ClosureExporter)->exportClosure($closure);
+}
 
-    $reader = static function(string $filename, int $start = null, int $end = null): array {
-        $fh = fopen($filename, 'r');
-        $num = 0;
-        $lines = [];
-
-        while (!feof($fh)) {
-            $line = fgets($fh);
-            $num++;
-
-            if ($start != null) {
-                if ($num >= $start) {
-                    $lines[$num] = $line;
-                }
-
-                if ($end != null && $end == $num) {
-                    break;
-                }
-
-                continue;
-            }
-
-            $lines[$num] = $line;
-
-            if ($end != null && $end == $num) {
-                break;
-            }
-        }
-
-        fclose($fh);
-
-        return $lines;
-    };
-
-    $code = $reader($reflector->getFileName(), $reflector->getStartLine(), $reflector->getEndLine());
-    $code = implode('', $code);
-
-    if (!str_starts_with($code, '<?') && !str_starts_with($code, '<?php')) {
-        $code = '<?php ' . $code;
-    }
-
-    $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
-
-    $ast = $parser->parse($code);
-
-    $nodeFinder = new NodeFinder();
-    
-    $node = $nodeFinder->findFirst($ast,
-        static fn(Node $node) => $node instanceof A || $node instanceof C
-    );
-
-    return (new Standard)->prettyPrintExpr($node);
+/**
+ * @param array $var
+ * @return string
+ * @throws ArrayExportException
+ */
+function export_array(array $var): string
+{
+    return (new ArrayExporter)->exportArray($var);
 }
