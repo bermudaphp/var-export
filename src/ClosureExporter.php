68f2581d3f92c5ab61f1bbbf999ee24517c4f7b1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-08 23:34:38 +0200

    Update ClosureExporter.php

--- src/ClosureExporter.php
+++ src/ClosureExporter.php
@@ -2,9 +2,12 @@
 
 namespace Bermuda\VarExport;
 
+use PhpParser\Builder\Function_;
 use PhpParser\Node;
 use PhpParser\NodeFinder;
 use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\FindingVisitor;
+use PhpParser\NodeVisitor\FirstFindingVisitor;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\Parser;
 use PhpParser\ParserFactory;
@@ -14,25 +17,49 @@ use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Closure as NodeClosure;
 use PhpParser\Node\Name;
 
-class ClosureExporter implements ClosureExporterInterface
+final class ClosureExporter implements ClosureExporterInterface
 {
-    protected static NodeFinder|null $nodeFinder = null;
+    private static NodeFinder|null $nodeFinder = null;
     public function exportClosure(\Closure $closure): string
     {
         $code = $this->readFile(($reflector = new \ReflectionFunction($closure))
             ->getFileName());
         $ast = $this->createParser()->parse($code);
 
-        $node = static::getFinder()->findFirst($ast, $this->createFindCallback($reflector));
+        $node = static::find($ast, $this->createFindCallback($reflector));
+
+        if (!$node) {
+            throw new ExportException("Can't export closure");
+        }
+
+        $visitor = $this->getVisitor(
+            $this->getNamespace($ast),
+            $this->getUseNodes($ast),
+            $this->findClassName(...),
+            $reflector->getFileName()
+        );
 
         $traverser = new NodeTraverser;
-        $traverser->addVisitor($this->getVisitor($this->getNamespace($ast), $this->getUseNodes($ast)));
+        $traverser->addVisitor($visitor, $this->getUseNodes($ast));
         $traverser->traverse([$node]);
 
         return $this->getPrinter()->prettyPrintExpr($node);
     }
+
+    private function findClassName(Node $node):? Node
+    {
+        if ($node instanceof Node\Stmt\Class_) {
+            return $node;
+        };
+
+        $nextNode = $node->getAttribute('parent');
+        if (!$nextNode) {
+            return null;
+        }
+        return $this->findClassName($nextNode);
+    }
     
-    protected function readFile(string $filename): string {
+    private function readFile(string $filename): string {
         $fh = fopen($filename, 'r');
         $content = '';
 
@@ -43,23 +70,28 @@ class ClosureExporter implements ClosureExporterInterface
         fclose($fh);
         return $content;
     }
+
+    private static function getFinder(): NodeFinder
+    {
+        return static::$nodeFinder ?? static::$nodeFinder = new NodeFinder;
+    }
     
-    protected function createParser(): Parser
+    private function createParser(): Parser
     {
         return (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
     }
 
-    protected function getReflector(\Closure $closure): \ReflectionFunction
+    private function getReflector(\Closure $closure): \ReflectionFunction
     {
         return new \ReflectionFunction($closure);
     }
 
-    protected function getPrinter(): PrettyPrinterAbstract
+    private function getPrinter(): PrettyPrinterAbstract
     {
         return (new Standard);
     }
 
-    protected function createFindCallback(\ReflectionFunction $reflector): callable
+    private function createFindCallback(\ReflectionFunction $reflector): callable
     {
         return static function(Node $node) use ($reflector): bool {
             return ($node instanceof ArrowFunction || $node instanceof NodeClosure)
@@ -67,17 +99,59 @@ class ClosureExporter implements ClosureExporterInterface
         };
     }
 
-    protected static function getFinder(): NodeFinder
+    /**
+     * @param $nodes
+     * @param callable $filter
+     * @return Node
+     */
+    private static function find($nodes, callable $filter):? Node
     {
-        return static::$nodeFinder ?? static::$nodeFinder = new NodeFinder;
+        if (!is_array($nodes)) {
+            $nodes = [$nodes];
+        }
+
+        $visitor = new class($filter) extends FirstFindingVisitor {
+            /**
+             * @var Node[]
+             */
+            private $stack = [];
+            public function enterNode(Node $node)
+            {
+                if (!empty($this->stack)) {
+                    $node->setAttribute('parent', $this->stack[count($this->stack) - 1]);
+                }
+
+                $this->stack[] = $node;
+
+                return parent::enterNode($node);
+            }
+
+            public function beforeTraverse(array $nodes)
+            {
+                $this->stack = [];
+                return parent::beforeTraverse($nodes);
+            }
+
+            public function leaveNode(Node $node)
+            {
+                array_pop($this->stack);
+                return parent::leaveNode($node);
+            }
+        };
+
+        $traverser = new NodeTraverser;
+        $traverser->addVisitor($visitor);
+        $traverser->traverse($nodes);
+
+        return $visitor->getFoundNode();
     }
 
-    protected function getNamespace(array $ast):? Node\Stmt\Namespace_
+    private function getNamespace(array $ast):? Node\Stmt\Namespace_
     {
         return static::getFinder()->findFirstInstanceOf($ast, Node\Stmt\Namespace_::class);
     }
 
-    protected function getUseNodes(array $ast):? array
+    private function getUseNodes(array $ast):? array
     {
         $ast = static::getFinder()->find($ast, static fn($node) => $node instanceof Node\Stmt\Use_
             || $node instanceof Node\Stmt\GroupUse
@@ -86,17 +160,22 @@ class ClosureExporter implements ClosureExporterInterface
         return $ast == [] ? null : $ast;
     }
 
-    protected function getVisitor(?Node\Stmt\Namespace_ $namespace, ?array $uses)
+    private function getVisitor(?Node\Stmt\Namespace_ $namespace, ?array $uses, callable $clsNameFinder, string $file): NodeVisitorAbstract
     {
-        return new class($namespace, $uses) extends NodeVisitorAbstract
+        return new class($namespace, $uses, $clsNameFinder, $file) extends NodeVisitorAbstract
         {
             /**
              * @var Node[]
              */
             private $stack = [];
+            private $clsNameFinder;
             public function __construct(
                 private ?Node\Stmt\Namespace_ $namespace,
-                private ?array $uses) {
+                private ?array $uses,
+                callable $clsNameFinder,
+                private string $file
+            ) {
+                $this->clsNameFinder = $clsNameFinder;
             }
 
             public function beforeTraverse(array $nodes)
@@ -106,6 +185,8 @@ class ClosureExporter implements ClosureExporterInterface
 
             public function enterNode(Node $node)
             {
+                static $cls = null;
+
                 if (!empty($this->stack)) {
                     $node->setAttribute('parent', $this->stack[count($this->stack) - 1]);
                 }
@@ -155,12 +236,36 @@ class ClosureExporter implements ClosureExporterInterface
                     }
 
                     if ($this->namespace) {
+                        foreach ($node->parts as &$part) {
+                            if (strtolower($part) == 'self' || strtolower($part) == 'static') {
+                                if (!$cls) $cls = ($this->clsNameFinder)($node);
+                                if ($cls) $part = $cls->name->name;
+                            }
+                        }
+
                         return new Name\FullyQualified(
                             [...$this->namespace->name->parts, ...$node->parts],
                             $node->getAttributes()
                         );
                     }
                 }
+
+                if ($node instanceof Node\Scalar\MagicConst\Class_) {
+                    if (!$cls) $cls = ($this->clsNameFinder)($node);
+                    if ($cls) {
+                        $part = $cls->name->name;
+                        return new Node\Expr\ClassConstFetch(new Name\FullyQualified([...$this->namespace->name->parts, $part]), 'class',
+                            $node->getAttributes()
+                        );
+                    }
+
+                    return $node;
+                }
+
+                if ($node instanceof Node\Scalar\MagicConst\File) {
+                    return new Node\Scalar\String_($this->file);
+                }
+
 
                 return $node;
             }
