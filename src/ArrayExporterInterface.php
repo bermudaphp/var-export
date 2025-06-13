6dea2c98960ed33920d9d0f77b66ac9e84877a77
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-06-13 07:52:37 +0200

    Update ArrayExporterInterface.php

--- src/ArrayExporterInterface.php
+++ src/ArrayExporterInterface.php
@@ -4,5 +4,12 @@ namespace Bermuda\VarExport;
 
 interface ArrayExporterInterface
 {
+    /**
+     * Export array to its string representation with semicolon.
+     *
+     * @param array $var The array to export
+     * @return string The exported array as PHP code with semicolon
+     * @throws ArrayExportException If array cannot be exported
+     */
     public function exportArray(array $var): string ;
 }
