3d53e295783086bd6377fff2d9bfd5a4e940906d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-06-13 07:53:36 +0200

    Update ClosureExporterInterface.php

--- src/ClosureExporterInterface.php
+++ src/ClosureExporterInterface.php
@@ -4,5 +4,12 @@ namespace Bermuda\VarExport;
 
 interface ClosureExporterInterface
 {
+    /**
+     * Export closure to its string representation.
+     *
+     * @param \Closure $closure The closure to export
+     * @return string The exported closure as a string
+     * @throws ExportException If closure cannot be exported
+     */
     public function exportClosure(\Closure $closure): string ;
 }
